f9ecc33d77c50e4b7e20e72e6c6c3104fc6a6156
samarthsarkar <samarthsarkar30@gmail.com>
2022-06-03 10:40:41 +0400

    dashboard update
    update for dashboard- connectivity to login api and abstraction from manual access

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -1,11 +1,18 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['userdata'])){
+        header("location: ../");
+    }
+?>
 <html>
     <head>
         <title>Dashboard</title>
+        <link rel="stylesheet" href="../css/home.css">
     </head>
         <body>
             <button>Back</button>
-            <button>Logout</button>
-            <h1>Online Voting System</h1>
+            <button id="logout">Logout</button>
+            <h1>USER DASHBOARD</h1>
             <hr>
             <div id="Profile"></div>
             <div id="Group"></div>
