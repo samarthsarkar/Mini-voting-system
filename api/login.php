3febc8447328d9195db9230d9afe376ab54bb5d5
samarthsarkar <samarthsarkar30@gmail.com>
2022-06-03 10:37:32 +0400

    login backend
    uploading login php file which enables connectivity from web page to database table user

---
+++ api/login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include("connect.php");
+
+$mobile = $_POST['mobile'];
+$password = $_POST['password'];
+$role = $_POST['role'];
+
+$check = mysqli_query($connect, "SELECT * FROM user WHERE mobile='$mobile' AND password='$password'");
+
+if(mysqli_num_rows($check)>0){
+    $userdata = mysqli_fetch_array($check);
+    $groups = mysqli_query($connect, "SELECT * FROM user WHERE role=2");
+    $groupdata = mysqli_fetch_all($groups, mysqli_assoc);
+
+    $_SESSION['userdata'] = $userdata;
+    $_SESSION['groupdata'] = $groupdata;
+
+    echo'
+    <script>
+        window.location = "../routes/dashboard.php";
+    </script>
+    ';
+}
+else{
+    echo'
+            <script>
+                alert("The Username or Password is incorrect!");
+                window.location = "../";
+            </script>
+            ';
+}
+?>
